11f15a1e961dcf34571043879e27cd49edeea1af
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-02-15 13:52:47 +0000

    Deploying to gh-pages from @ DenysLes/goit-js-hw-09@c319a63b073d8f1b9deb9fa0d430e704a95da333 🚀

---
+++ 01-color-switcher.61f75e58.js
@@ -0,0 +1,2 @@
+var t,e,r,a;t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]"),a=null,r.setAttribute("disabled",""),e.addEventListener("click",function(e){e.target.setAttribute("disabled",!0),r.removeAttribute("disabled"),a=setInterval(function(){t.style.backgroundColor="#".concat(Math.floor(0xffffff*Math.random()).toString(16))},1e3)}),r.addEventListener("click",function(t){t.target.setAttribute("disabled",!0),e.removeAttribute("disabled"),clearInterval(a)});
+//# sourceMappingURL=01-color-switcher.61f75e58.js.map
